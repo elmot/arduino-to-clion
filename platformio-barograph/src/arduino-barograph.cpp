0b07b708f4e650de2e63590fde35281193ec4aa6
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2020-04-21 22:24:26 +0300

    A little of refactoring

--- platformio-barograph/src/arduino-barograph.cpp
+++ platformio-barograph/src/arduino-barograph.cpp
@@ -23,6 +23,10 @@ void setup() {
   /* This clears the SRAM of the e-paper display */
 }
 
+char buf[50];
+
+void drawString(const String &s, int x, int y);
+
 void loop() {
   float pressure = 0.0075f * bmp.readPressure();
 
@@ -34,33 +38,41 @@ void loop() {
 
   Serial.println(s);
 
-  char buf[50];
-  s.toCharArray(buf, 24);
-  /**
-      Due to RAM not enough in Arduino UNO, a frame buffer is not allowed.
-      In this case, a smaller image buffer is allocated and you have to
-      update a partial display several times.
-      1 byte = 8 pixels, therefore you have to set 8*N pixels at a time.
-      Note that the picture on the display is 270 degree rotated 
-      and paint size and coorinates calculations are a bit tricky
-  */
-  int txtWidth = Font24.Width * s.length();
-  if (txtWidth > 300) txtWidth = 300;
+    if (epd.Init() != 0) {
+        Serial.print("e-Paper init failed");
+        return;
+    }
+    epd.ClearFrame();
 
-  Paint paint = Paint(image, txtWidth, 24);
-  paint.Clear(WHITE);
-  paint.DrawStringAt(0, 0,  buf, &Font24, BLACK);
-  if (epd.Init() != 0) {
-    Serial.print("e-Paper init failed");
-    return;
-  }
-  epd.ClearFrame();
-  epd.SetPartialWindow(paint.GetImage(), 0, 0, paint.GetWidth(), paint.GetHeight());
+    drawString(s, 0, 0);
+    s = "T=";
+    s += String(bmp.readTemperature(), 1);
+    s += " C";
+    drawString(s, 0, 28);
 
-  /* This displays the data from the SRAM in e-Paper module */
+    /* This displays the data from the SRAM in e-Paper module */
   epd.DisplayFrame();
   /* Deep sleep */
   epd.Sleep();
 
   delay(15000);
+}
+
+void drawString(const String &s, int x, int y) {
+    s.toCharArray(buf, 24);
+    /**
+          Due to RAM not enough in Arduino UNO, a frame buffer is not allowed.
+          In this case, a smaller image buffer is allocated and you have to
+          update a partial display several times.
+          1 byte = 8 pixels, therefore you have to set 8*N pixels at a time.
+          Note that the picture on the display is 270 degree rotated 
+          and paint size and coorinates calculations are a bit tricky
+      */
+    int txtWidth = Font24.Width * s.length();
+    if (txtWidth > 300) txtWidth = 300;
+
+    Paint paint = Paint(image, txtWidth, 24);
+    paint.Clear(WHITE);
+    paint.DrawStringAt(0, 0,  buf, &Font24, BLACK);
+    epd.SetPartialWindow(paint.GetImage(), x, y, paint.GetWidth(), paint.GetHeight());
 }
