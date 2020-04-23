9d1a6d63d1acd440ebe49b47e1bc10b15d50f6a0
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2020-04-23 22:18:56 +0300

    Pressure sensor test

--- stm32-barograph/Core/Src/cppMain.cpp
+++ stm32-barograph/Core/Src/cppMain.cpp
@@ -1,33 +1,45 @@
 //
 // Created by Ilia.Motornyi on 22/04/2020.
 //
-#include <rtc.h>
-#include "main.h"
-//#include <iostream>
 #include <cstdio>
-#include <adc.h>
+#include "rtc.h"
+#include "i2c.h"
+#include "main.h"
+#include "adc.h"
+#include "Adafruit_BMP085_Stm32.h"
+
+Adafruit_BMP085 bmp{hi2c1};
+const uint16_t *const VREFINT_CAL = (const uint16_t *) (uintptr_t) 0x1FFFF7BA;
+volatile bool timeToGo = true;
 
 __attribute__((noreturn)) void cppMain() {
-    HAL_SuspendTick();
+    if (!bmp.begin()) {
+        reportError("BMP085 initialization error\r\n");
+    }
     HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED);
-    while(HAL_ADC_GetState(&hadc1) == HAL_ADC_STATE_BUSY_INTERNAL);
+    while (HAL_ADC_GetState(&hadc1) == HAL_ADC_STATE_BUSY_INTERNAL);
     HAL_ADC_Start(&hadc1);
     while (true) {
-        __WFI();
+        while (!timeToGo) {
+            __WFI();
+        }
+        timeToGo = false;
+        RTC_TimeTypeDef time;
+        RTC_DateTypeDef date;
+        HAL_RTC_GetDate(&hrtc, &date, RTC_FORMAT_BIN);
+        HAL_RTC_GetTime(&hrtc, &time, RTC_FORMAT_BIN);
+
+        printf("Date: %04d-%02d-%02d %02d:%02d:%02d.%03ld\r\n", date.Year + 2000, date.Month, date.Date,
+               time.Hours, time.Minutes, time.Seconds, 1000 * time.SubSeconds / (time.SecondFraction + 1));
+        uint16_t v = HAL_ADC_GetValue(&hadc1);
+        int voltage = 3300L * *VREFINT_CAL / v;
+        float temperature = bmp.readTemperature();
+        float pressure = 0.0075f * bmp.readPressure();
+        printf("VCC: %d mV; P: %.1f mmHg; T: %.2f C; \n\r", voltage, pressure, temperature);
     }
 }
 
-const uint16_t VREFINT_CAL = *(uint16_t*)((uintptr_t)0x1FFFF7BA);
-void HAL_RTCEx_WakeUpTimerEventCallback(RTC_HandleTypeDef *hrtc) {
-    RTC_TimeTypeDef time;
-    RTC_DateTypeDef date;
-    HAL_RTC_GetDate(hrtc, &date, RTC_FORMAT_BIN);
-    HAL_RTC_GetTime(hrtc, &time, RTC_FORMAT_BIN);
-
-    printf("Date: %04d-%02d-%02d %02d:%02d:%02d.%03ld\r\n", date.Year + 2000, date.Month, date.Date,
-           time.Hours, time.Minutes, time.Seconds, 1000 * time.SubSeconds / (time.SecondFraction + 1));
-    uint32_t state = HAL_ADC_GetState(&hadc1);
-    uint16_t v = HAL_ADC_GetValue(&hadc1);
-    int voltage = 3300L * VREFINT_CAL / v;
-    printf("ADC State: %x; value: %d; voltage: %d mV", state, v, voltage);
+void HAL_RTCEx_WakeUpTimerEventCallback(RTC_HandleTypeDef *aHrtc) {
+    UNUSED(aHrtc);
+    timeToGo = true;
 }
--- stm32-barograph/Core/Src/main.c
+++ stm32-barograph/Core/Src/main.c
@@ -129,11 +129,11 @@ void SystemClock_Config(void)
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Configure LSE Drive Capability
+  /** Configure LSE Drive Capability 
   */
   HAL_PWR_EnableBkUpAccess();
   __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
-  /** Initializes the CPU, AHB and APB busses clocks
+  /** Initializes the CPU, AHB and APB busses clocks 
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSE;
   RCC_OscInitStruct.LSEState = RCC_LSE_ON;
@@ -144,7 +144,7 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB busses clocks
+  /** Initializes the CPU, AHB and APB busses clocks 
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
@@ -219,7 +219,7 @@ void Error_Handler(void)
   * @retval None
   */
 void assert_failed(uint8_t *file, uint32_t line)
-{
+{ 
   /* USER CODE BEGIN 6 */
    reportError("Wrong parameters value: file %s on line %ld\r\n", file, line);
   /* USER CODE END 6 */
