1219118daa408faff7c8e526cf9f3e48a40d46be
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2020-04-29 22:27:59 +0300

    Proper timing

--- stm32-barograph/Core/Src/rtc.c
+++ stm32-barograph/Core/Src/rtc.c
@@ -45,7 +45,7 @@ void MX_RTC_Init(void)
   }
   /** Enable the WakeUp 
   */
-  if (HAL_RTCEx_SetWakeUpTimer_IT(&hrtc, 13, RTC_WAKEUPCLOCK_CK_SPRE_16BITS) != HAL_OK)
+  if (HAL_RTCEx_SetWakeUpTimer_IT(&hrtc, 60, RTC_WAKEUPCLOCK_CK_SPRE_16BITS) != HAL_OK)
   {
     Error_Handler();
   }
