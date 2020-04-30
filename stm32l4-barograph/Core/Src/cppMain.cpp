a40438890d8df2a9f0661c0fcae9e100432dd38b
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2020-05-01 01:41:06 +0300

    Take display into use

--- stm32l4-barograph/Core/Src/cppMain.cpp
+++ stm32l4-barograph/Core/Src/cppMain.cpp
@@ -1,14 +1,58 @@
 #include <cstdio>
+#include <cstring>
 #include "rtc.h"
 #include "i2c.h"
 #include "main.h"
 #include "adc.h"
 #include "Adafruit_BMP085_Stm32.h"
+#include "fonts.h"
+#include "epd4in2.h"
+#include "epdpaint.h"
 
 Adafruit_BMP085 bmp{hi2c1};
 volatile bool timeToGo = true;
 
-__attribute__((noreturn)) void cppMain() {
+unsigned char image[EPD_WIDTH * EPD_HEIGHT / 8];
+Epd epd;
+
+#define WHITE 1
+#define BLACK 0
+
+void drawString(const char *s, int x, int y) {
+  unsigned int txtWidth = Font24.Width * strlen(s);
+  if (txtWidth > 300)
+    txtWidth = 300;
+
+  Paint paint = Paint(image, (int)txtWidth, 24);
+  paint.Clear(WHITE);
+  paint.DrawStringAt(0, 0, s, &Font24, BLACK);
+  epd.SetPartialWindow(paint.GetImage(), x, y, paint.GetWidth(),
+                       paint.GetHeight());
+}
+
+void drawData(float pressure, float temperature) {
+  char buff[100];
+  snprintf(buff, sizeof(buff), "P=%.2f mmHg", pressure);
+
+  if (epd.Init() != 0) {
+    reportError("e-Paper init failed\r\n");
+    return;
+  }
+  epd.ClearFrame(false);
+
+  drawString(buff, 0, 0);
+  snprintf(buff, sizeof(buff), "T=%.2f C", temperature);
+  drawString(buff, 0, 28);
+
+/* This displays the data from the SRAM in e-Paper module */
+  epd.DisplayFrame();
+/* Deep sleep */
+  epd.Sleep();
+
+}
+
+__attribute__((noreturn))
+void cppMain() {
   if (!bmp.begin()) {
     reportError("BMP085 initialization error\r\n");
   }
@@ -29,10 +73,14 @@ __attribute__((noreturn)) void cppMain() {
            time.Hours, time.Minutes, time.Seconds, 1000 * time.SubSeconds / (time.SecondFraction + 1));
     while (HAL_ADC_GetState(&hadc1) == HAL_ADC_STATE_BUSY_INTERNAL);
     uint16_t v = HAL_ADC_GetValue(&hadc1);
-    int voltage = __LL_ADC_CALC_VREFANALOG_VOLTAGE(v,LL_ADC_RESOLUTION_12B);
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "hicpp-signed-bitwise"
+    int voltage = __HAL_ADC_CALC_VREFANALOG_VOLTAGE(v,LL_ADC_RESOLUTION_12B);
+#pragma clang diagnostic pop
     float temperature = bmp.readTemperature();
     float pressure = 0.0075f * bmp.readPressure();
     printf("VCC: %d mV; P: %.1f mmHg; T: %.2f C; \n\r", voltage, pressure, temperature);
+    drawData(pressure, temperature);
   }
 }
 
---
+++ stm32l4-barograph/Drivers/epm4in2/Src/epdif.cpp
@@ -0,0 +1,65 @@
+/**
+ *  @filename   :   epdif.cpp
+ *  @brief      :   Implements EPD interface functions
+ *                  Users have to implement all the functions in epdif.cpp
+ *  @author     :   Yehui from Waveshare
+ *
+ *  Copyright (C) Waveshare     August 10 2017
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documnetation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to  whom the Software is
+ * furished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "epdif.h"
+#include "spi.h"
+
+EpdIf::EpdIf() = default;
+
+void EpdIf::DigitalWriteDC(bool value) {
+    //todo bidband
+    HAL_GPIO_WritePin(DISP_DC_GPIO_Port, DISP_DC_Pin, value ? GPIO_PIN_SET : GPIO_PIN_RESET);
+}
+
+void EpdIf::DigitalWriteCS(bool value) {
+    //todo bidband
+    HAL_GPIO_WritePin(DISP_CS_GPIO_Port, DISP_CS_Pin, value ? GPIO_PIN_SET : GPIO_PIN_RESET);
+}
+
+void EpdIf::DigitalWriteRST(bool value) {
+    //todo bidband
+    HAL_GPIO_WritePin(DISP_RST_GPIO_Port, DISP_RST_Pin, value ? GPIO_PIN_SET : GPIO_PIN_RESET);
+}
+
+int EpdIf::DigitalReadBusy() {
+    //todo bidband
+    return HAL_GPIO_ReadPin(DISP_BUSY_GPIO_Port, DISP_BUSY_Pin);
+}
+
+void EpdIf::DelayMs(unsigned int delaytime) {
+    HAL_Delay(delaytime);
+}
+
+void EpdIf::SpiTransfer(unsigned char data) {
+    DigitalWriteCS(false);
+    halError(__FUNCTION__, HAL_SPI_Transmit(&hspi1, &data, 1, 100));
+    DigitalWriteCS(true);
+}
+
+int EpdIf::IfInit() {
+    return 0;
+}
